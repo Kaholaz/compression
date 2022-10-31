b8f081658314d70cc2b63baaf41634a816f41313
Kaholaz <nim.nimern@gmail.com>
2022-10-31 17:30:43 +0100

    add multiple tests

--- compression.py
+++ compression.py
@@ -28,17 +28,56 @@ def decode(text: bytearray) -> bytearray:
 def decode_file(file_name: str) -> bytearray:
     return decode(read_file(file_name))
 
+
 def decode_and_write_file(file_name: str):
     write_file(file_name + ".uncompressed", decode_file(file_name))
 
 
-if __name__ == "__main__":
+def easy():
+    text = """
+    Jeg gikk en tur på stien
+    og søkte skogens ro.
+    Da hørte jeg fra lien
+    en gjøk som gol ko-ko.
+    Ko-ko, ko-ko, ko-ko, ko-ro, ko-ko
+    Ko-ko, ko-ko, ko-ko, ko-ro, ko-ko
+
+    Jeg spurte den hvor mange,
+    hvor mange år ennå.
+    Den svarte meg med lange
+    og klagende ko-ko.
+    Ko-ko, ko-ko ....
+
+    Jeg spurte om dens make
+    og om dens eget bo. 
+    Den satt der oppå grenen
+    og kikket ned og lo.
+    Ko-ko, ko-ko ....
+
+    Vi bygger ikke rede,
+    vi har hjem, vi to.
+    Fru Spurv er mor til barna
+    vi galer kun ko-ko"
+    Ko-ko, ko-ko....
+    """
+
+    print(text := bytearray(text, "utf-8"))
+    print(encoded := encode(text))
+    print(decoded := decode(encoded))
+    assert decoded == text
+
+
+def hard():
     files = [
+        "opg8-kompr.pdf",
+        "diverse.txt",
         "diverse.lyx",
-        "diverse.pdf",
-        "diverse.txt"
-        "oppg8-kompr.pdf"
     ]
-    for file in tqdm(files):
+
+    for file in files:
         encode_file(file)
         decode_and_write_file(file + ".compressed")
+
+
+if __name__ == "__main__":
+    hard()
